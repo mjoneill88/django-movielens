d0161f18dd18debab31277409fe693fbc647ed8d
gcrowder <gregory.j.crowder@gmail.com>
2016-05-05 22:39:22 -0400

    Add links to users and movies in repsective templates.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # django-movielens
 This project is an exercise in utilizing the small Movie Lens dataset in a django web app.
+
+## [models.py](https://github.com/mjoneill88/django-movielens/blob/master/movie_server/movie_recommender/models.py)
+We utilize django's model paradigm to save our data in a postgresql database.
+
+## 
--- movie_server/movie_recommender/views.py
+++ movie_server/movie_recommender/views.py
@@ -20,16 +20,16 @@ def movie(request, movie_id):
     rating_list = Rating.objects.filter(movie_id=movie.id)
     movie_average = Rating.objects.filter(movie_id=movie.id).aggregate(Avg('rating'))
     template = loader.get_template('movie_recommender/movie.html')
-    context = {'movie' : movie,
-                'rating_list' : rating_list,
-                'average': movie_average['rating__avg']}
+    context = {'movie': movie,
+               'rating_list': rating_list,
+               'average': movie_average['rating__avg']}
     return HttpResponse(template.render(context, request))
 
 def user(request, user_id):
     rater = Rater.objects.get(user_id=user_id)
     rating_list = Rating.objects.filter(user_id=user_id)
     template = loader.get_template('movie_recommender/user.html')
-    context = {'rater' : rater,
-                'rating_list' : rating_list}
+    context = {'rater': rater,
+               'rating_list': rating_list}
     return HttpResponse(template.render(context, request))
 # Create your views here.
