018217b82a26f514c6bbdc9e965707044396fb00
gcrowder <gregory.j.crowder@gmail.com>
2016-05-05 21:56:22 -0400

    Update README. Add str methods to models.

---
+++ README.md
@@ -0,0 +1,2 @@
+# django-movielens
+This project is an exercise in utilizing the small Movie Lens dataset in a django web app.
--- movie_server/movie_recommender/models.py
+++ movie_server/movie_recommender/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Movie(models.Model):
     movie_id = models.IntegerField()
     title = models.CharField(max_length=200)
-    
+
+    def __str__(self):
+        return "{}: {}".format(self.movie_id, self.title)
 
 
 class Rater(models.Model):
@@ -14,10 +16,18 @@ class Rater(models.Model):
     occupation = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=5)
 
+    def __str__(self):
+        return "Rater ID: {}".format(self.user_id)
+
 
 class Rating(models.Model):
     user_id = models.ForeignKey(Rater, on_delete=models.CASCADE)
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     rating = models.IntegerField()
 
+    def __str__(self):
+        return "{}: {}\nUser {} : Rating {}".format(self.movie_id.movie_id,
+                                                    self.movie_id.title,
+                                                    self.user_id.user_id,
+                                                    self.rating)
 # Create your models here.
